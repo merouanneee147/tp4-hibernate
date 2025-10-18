cd5e49423b0a36c57258d48413fb735867595ae7
merouanneee147 <mechachmarouane@gmail.com>
2025-10-18 01:02:04 +0100

    first commit

---
+++ src/main/java/entities/Salle.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+    // Constructeurs, getters et setters
+
+    public Salle(int id, String code, List<Machine> machines) {
+        this.id = id;
+        this.code = code;
+        this.machines = machines;
+    }
+
+    public Salle(String a1) {
+    }
+
+    public Salle() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+}
